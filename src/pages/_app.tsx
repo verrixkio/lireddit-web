9772d13cc67958c3778df6936da061755460c8d5
Thomas <verrixkio@gmail.com>
2021-08-31 09:45:27 -0600

    update container js to tsx file and organize app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
 import { Provider, createClient } from 'urql'
+import theme from '../theme'
 
 const client = createClient({
   url: 'http://localhost:4000/graphql',
@@ -8,7 +9,6 @@ const client = createClient({
   },
 });
 
-import theme from '../theme'
 
 function MyApp({ Component, pageProps }) {
   return (
