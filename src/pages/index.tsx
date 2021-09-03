d3fcc28097dd907c77cebea634a05734518dbff1
Thomas <verrixkio@gmail.com>
2021-09-03 14:06:18 -0600

    add server side rendering to application using urql. Also refactor our client setup into modular files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import { NavBar } from "../components/NavBar";
+import { withUrqlClient } from 'next-urql';
+import { createUrqlClient } from "../utils/createUrqlClients";
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => (
-  <div>
-    <NavBar></NavBar>
-    Hello Worlds
-  </div>
-)
-
-export default Index
+const Index = () => {
+  const [{ data }] = usePostsQuery()
+  return (
+    <>
+      <NavBar />
+      <div> Hello Worlds</div>
+      <br></br>
+      {!data ? null : data.posts.map(p => <div key={p.id}>{p.title}</div>)}
+    </>
+  )
+}
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index)
